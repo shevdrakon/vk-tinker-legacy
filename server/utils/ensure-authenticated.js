4f6ebfa4b58b4efced85aeea2370fae6dd55076f
vadimS <spieler3@yandex.ru>
2017-03-06 18:27:46 +0300

    ensure-auth mw

--- server/utils/ensure-authenticated.js
+++ server/utils/ensure-authenticated.js
@@ -1,15 +1,9 @@
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
-function ensureAuthenticated(req, res, next) {
+module.exports = function (req, res, next) {
     if (req.isAuthenticated())
         return next()
 
-    res.redirect('/login')
+    const error = new Error('Unauthorized.')
+    error.status = 401
+    error.response = response
+    throw error
 }
-
-module.exports = {
-    ensureAuthenticated
-};
